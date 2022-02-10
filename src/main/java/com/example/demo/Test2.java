54a6a86315b7b9e897fb0721ddcc4a9adafe09c4
Kristina <kitty_kris@bk.ru>
2022-02-10 23:56:33 +0300

    add third test class in 123

---
+++ src/main/java/com/example/demo/Test2.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Test2 {
+    private int year;
+    private int age;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int sum(int year, int age) {
+        return year + age;
+    }
+}
