9c395646fb874dd6da0e69ec8e58c36e75350ecd
Kristina <kitty_kris@bk.ru>
2022-02-11 15:09:49 +0300

    add new task to class Test4

--- src/main/java/com/example/demo/Test4.java
+++ src/main/java/com/example/demo/Test4.java
@@ -4,4 +4,7 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("Hey!");
     }
+    public void print() {
+        System.out.println("Hey Hey!");
+    }
 }
